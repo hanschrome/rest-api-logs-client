4d000677e696ce134133158d5d752bfc0da89cde
Hans <hanscastroadmn@gmail.com>
2020-06-08 21:02:06 +0200

    Added Log to RestApiLogsClient interface

--- lib/RestApiLogsClient/Common/Client/RestApiLogsClient.php
+++ lib/RestApiLogsClient/Common/Client/RestApiLogsClient.php
@@ -3,11 +3,17 @@
 
 namespace RestApiLogsClient\Common\Client;
 
+use RestApiLogsClient\Common\Log\Log;
+
 /**
  * Interface RestApiLogsClient
  * @package RestApiLogsClient\Common\Client
  */
 interface RestApiLogsClient
 {
-
+    /**
+     * @param Log $log
+     * @return bool
+     */
+    public function log(Log $log): bool;
 }
