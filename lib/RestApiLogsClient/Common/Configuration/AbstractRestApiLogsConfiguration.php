4756942ee26d8d5135fc4dd2a406f5bd3f3eec48
Hans <hanscastroadmn@gmail.com>
2020-06-08 23:09:51 +0200

    Configuration fields

--- lib/RestApiLogsClient/Common/Configuration/AbstractRestApiLogsConfiguration.php
+++ lib/RestApiLogsClient/Common/Configuration/AbstractRestApiLogsConfiguration.php
@@ -9,5 +9,66 @@ namespace RestApiLogsClient\Common\Configuration;
  */
 abstract class AbstractRestApiLogsConfiguration implements RestApiLogsConfiguration
 {
+    /**
+     * @var string
+     */
+    private string $domain;
+    /**
+     * @var string
+     */
+    private string $path = '/';
+    /**
+     * @var bool
+     */
+    private bool $ssl = true;
+
+    /**
+     * @return string
+     */
+    public function getDomain(): string
+    {
+        return $this->domain;
+    }
+
+    /**
+     * @param string $domain
+     */
+    public function setDomain(string $domain): void
+    {
+        $this->domain = $domain;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param string $path
+     */
+    public function setPath(string $path): void
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSsl(): bool
+    {
+        return $this->ssl;
+    }
+
+    /**
+     * @param bool $ssl
+     */
+    public function setSsl(bool $ssl): void
+    {
+        $this->ssl = $ssl;
+    }
+
 
 }
--- lib/RestApiLogsClient/Common/Configuration/RestApiLogsConfiguration.php
+++ lib/RestApiLogsClient/Common/Configuration/RestApiLogsConfiguration.php
@@ -9,5 +9,33 @@ namespace RestApiLogsClient\Common\Configuration;
  */
 interface RestApiLogsConfiguration
 {
+    /**
+     * @return string
+     */
+    public function getDomain(): string;
 
+    /**
+     * @param string $domain
+     */
+    public function setDomain(string $domain): void;
+
+    /**
+     * @return string
+     */
+    public function getPath(): string;
+
+    /**
+     * @param string $path
+     */
+    public function setPath(string $path): void;
+
+    /**
+     * @return bool
+     */
+    public function isSsl(): bool;
+
+    /**
+     * @param bool $ssl
+     */
+    public function setSsl(bool $ssl): void;
 }
