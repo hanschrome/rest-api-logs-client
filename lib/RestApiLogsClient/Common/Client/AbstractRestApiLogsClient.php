3820387dee34a6cc1d042c8125619db4f44b7c62
Hans <hanscastroadmn@gmail.com>
2020-06-09 00:12:34 +0200

    Curl http request

--- lib/RestApiLogsClient/Common/Client/AbstractRestApiLogsClient.php
+++ lib/RestApiLogsClient/Common/Client/AbstractRestApiLogsClient.php
@@ -62,6 +62,54 @@ abstract class AbstractRestApiLogsClient implements RestApiLogsClient
      * @return bool
      */
     private function logByHttp(array $data): bool {
-        return true;
+        /**
+         * Init curl
+         */
+        $curl = curl_init();
+
+        /**
+         * Post request
+         */
+        curl_setopt($curl, CURLOPT_POST, 1);
+
+        /**
+         * JSON data generic log
+         */
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+        /**
+         * Build url
+         */
+        $httpPrefix = $this->getRestApiLogsConfiguration()->isSsl() ? 'https' : 'http';
+
+        $url = $httpPrefix .
+            $this->getRestApiLogsConfiguration()->getDomain() .
+            $this->getRestApiLogsConfiguration()->getPath() .
+            'generic';
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        /**
+         * http headers
+         */
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json'
+        ));
+
+        /**
+         * Send & Evaluate output
+         */
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $out = curl_exec($curl);
+
+        if ($out) {
+            $out = json_decode($out, 1)['status'] === 'OK';
+        }
+
+        curl_close($curl);
+
+        /** Cast and return */
+        return (bool) $out;
     }
 }
